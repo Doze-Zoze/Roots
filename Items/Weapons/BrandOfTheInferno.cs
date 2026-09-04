d211df2d8d0ddda7fe7f621a536551a35ce861c7
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2026-09-04 04:44:31 -0600

    brand of the inferno graphics upgrade

--- Items/Weapons/BrandOfTheInferno.cs
+++ Items/Weapons/BrandOfTheInferno.cs
@@ -33,8 +33,9 @@ namespace RootsBeta.Items.Weapons
             item.noMelee = true;
             item.noUseGraphic = true;
             item.UseSound = null;
-
+            item.flame = true;
         }
+
         public override bool CanUseItem(Item item, Player player)
         {
             if (player.itemTime > 0 || player.ownedProjectileCounts[item.shoot] > 0)
@@ -53,10 +54,8 @@ namespace RootsBeta.Items.Weapons
             SwingWidth = 3.5f,
             RotationSpeed = 0.25f,
             CanDamage = false,
-            SwingOffsetAngle = (proj) =>
-            {
-                return proj.State.SwingWidth * -0.5f - 0.5f * (1 - MathF.Pow(proj.StateCompletion, 0.5f));
-            },
+            SwingOffsetAngle = proj =>
+                proj.State.SwingWidth * -0.5f - 0.5f * (1 - MathF.Pow(proj.StateCompletion, 0.5f)),
             OffsetDistance = 48,
             AlternateSwings = true
         };
@@ -76,24 +75,19 @@ namespace RootsBeta.Items.Weapons
             SwingWidth = 3.5f,
             CanDamage = false,
             RotationSpeed = 0.25f,
-            SwingOffsetAngle = (proj) =>
-            {
-                return proj.State.SwingWidth * 0.5f + 0.5f * MathF.Pow(proj.StateCompletion, 0.5f);
-            },
+            SwingOffsetAngle = proj =>
+                proj.State.SwingWidth * 0.5f + 0.5f * MathF.Pow(proj.StateCompletion, 0.5f),
             OffsetDistance = 48,
             AlternateSwings = true
         };
-
         public AttackState StrongStartup = new()
         {
             Time = 40,
             SwingWidth = 3.5f,
             RotationSpeed = 0.01f,
             CanDamage = false,
-            SwingOffsetAngle = (proj) =>
-            {
-                return proj.State.SwingWidth * -0.5f + (0.75f - 0.75f * MathF.Pow(proj.StateCompletion, 0.5f));
-            },
+            SwingOffsetAngle = proj => 
+                proj.State.SwingWidth * -0.5f + (0.75f - 0.75f * MathF.Pow(proj.StateCompletion, 0.5f)),
             OffsetDistance = 48
         };
         public AttackState StrongAttack = new()
@@ -104,10 +98,8 @@ namespace RootsBeta.Items.Weapons
             Loops = true,
             OffsetDistance = 48,
             Sound = SoundID.DD2_MonkStaffSwing,
-            SwingOffsetAngle = (proj) =>
-            {
-                return MathHelper.Lerp(-1.75f, proj.SwingWidth - 1.75f, proj.StateCompletion);
-            },
+            SwingOffsetAngle = proj =>
+                MathHelper.Lerp(-1.75f, proj.SwingWidth - 1.75f, proj.StateCompletion),
             AdditionalAI = (proj) =>
             {
                 if (proj.StateCompletion != 1) return;
@@ -123,13 +115,11 @@ namespace RootsBeta.Items.Weapons
             Time = 40,
             SwingWidth = 4.5f,
             CanDamage = true,
-            SwingOffsetAngle = (proj) =>
-            {
-                return MathHelper.Lerp(
+            SwingOffsetAngle = proj => 
+                MathHelper.Lerp(
                 MathHelper.Lerp(-1.75f, (MathHelper.TwoPi - 1.75f) * (40f / 12), proj.StateCompletion),
                 MathHelper.Lerp(-1.75f, proj.SwingWidth - 1.75f, proj.StateCompletion),
-                MathF.Pow(proj.StateCompletion, 0.45f));
-            },
+                MathF.Pow(proj.StateCompletion, 0.45f)),
             OffsetDistance = 48
         };
         public override List<AttackState> StateList => IsStrongSwing ? [StrongStartup, StrongAttack, StrongEndlag] : [Startup, Attack, Endlag];
@@ -138,6 +128,7 @@ namespace RootsBeta.Items.Weapons
         public override float LineCollisionLength => 100;
         public override int AfterImageCount => 15;
         private bool IsStrongSwing { get => Projectile.ai[0] == 1f; set => Projectile.ai[0] = value ? 1f : 0f; }
+
         public override void FakeOnSpawn()
         {
             if (Player.HasBuff(BuffID.ParryDamageBuff))
@@ -147,18 +138,51 @@ namespace RootsBeta.Items.Weapons
             }
             base.FakeOnSpawn();
         }
+
         public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
         {
             if (IsStrongSwing)
                 modifiers.SourceDamage *= BrandOfTheInferno.StrongSwingBonusDamage;
+            target.AddBuff(BuffID.OnFire3, 900);
             base.ModifyHitNPC(target, ref modifiers);
         }
+
         public static Asset<Texture2D> GlowMask => field ??= ModContent.Request<Texture2D>($"Terraria/Images/ItemFlame_{ItemID.DD2SquireDemonSword}");
+
         public override void PostDraw(Color lightColor)
         {
             Main.EntitySpriteDraw(GlowMask.Value, Projectile.Center - Main.screenPosition, GlowMask.Frame(),
                 Color.White with { A = 0}, Projectile.rotation, GlowMask.Size() * 0.5f, Projectile.scale,
                 Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally);
+
+            // PROBLEM: This does not render in deltatime. This probably renders at your framerate. doze please double check
+            
+            const int flameStacks = 7; //Same as vanilla
+
+            Vector2[] flamePos = new Vector2[7];
+            
+            for (int j = 0; j < flameStacks; j++)
+            {
+                flamePos[j].X = Main.rand.Next(-10, 11) * 0.15f;
+                flamePos[j].Y = Main.rand.Next(-10, 1) * 0.35f;
+            }
+
+            for (int i = 0; i < flameStacks; i++)
+            {
+                Vector2 offset = new(flamePos[i].X * Projectile.scale, flamePos[i].Y * Projectile.scale);
+                Main.EntitySpriteDraw(GlowMask.Value, Projectile.Center - Main.screenPosition + offset, GlowMask.Frame(),
+                    Color.White with { A = 0}, Projectile.rotation, GlowMask.Size() * 0.5f, Projectile.scale,
+                    Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally);
+            }
+            
+            Dust dust = Dust.NewDustDirect(
+                Projectile.TopLeft - (Player.direction == -1 ? new Vector2(Projectile.Size.X, 0) : Vector2.Zero) - (Projectile.Size * Projectile.scale * 0.5f),
+                (int)(Projectile.Size.X * Projectile.scale * 2), (int)(Projectile.Size.Y * Projectile.scale * 2), DustID.Torch,
+                Projectile.velocity.X * 0.2f + (Projectile.direction * 3), Projectile.velocity.Y * 0.2f, 100,
+                Color.Transparent, 0.7f);
+            dust.noGravity = true;
+            dust.velocity *= 2f;
+            dust.fadeIn = 0.9f;
         }
     }
 }
