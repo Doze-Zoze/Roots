0b0266fe48d2a0df9cfc76c5e74ab84727fac25c
Doze-Zoze <43045606+Doze-Zoze@users.noreply.github.com>
2026-09-13 04:26:25 -0500

    particle pass 1

--- Items/Weapons/ScourgeOfTheCorruptor.cs
+++ Items/Weapons/ScourgeOfTheCorruptor.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoMod.Logs;
 using Roots.Config;
 using RootsBeta.Utilities;
+using RootsCore;
 using RootsCore.ContentBaseClasses;
 using RootsCore.Extensions;
+using RootsCore.ParticleTextures;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using Terraria;
 using Terraria.Audio;
@@ -136,9 +136,9 @@ namespace RootsBeta.Items.Weapons
             AdditionalAI = proj =>
             {
 
-                var sortedProjectiles = (from p in Main.projectile 
-                    where p.active && p.hostile && p.damage > 0 
-                    select p).OrderByDescending(x => x.damage);
+                var sortedProjectiles = (from p in Main.projectile
+                                         where p.active && p.hostile && p.damage > 0
+                                         select p).OrderByDescending(x => x.damage);
 
                 var hitbox = proj.Player.Hitbox;
                 hitbox.Inflate(64, 64);
@@ -162,8 +162,9 @@ namespace RootsBeta.Items.Weapons
             CanDamage = false,
             OffsetDistance = 36,
             SwingOffsetAngle = proj =>
-                -proj.State.SwingWidth * ( -0.5f + (proj.StateCompletion)),
-            Startup = proj => {
+                -proj.State.SwingWidth * (-0.5f + (proj.StateCompletion)),
+            Startup = proj =>
+            {
                 proj.Angle = -proj.Player.DirectionTo(proj.Player.MouseWorld);
             },
             AlternateSwings = false,
@@ -184,19 +185,38 @@ namespace RootsBeta.Items.Weapons
             base.AdditionalAI();
         }
 
-        public void TriggerParry() 
+        public void TriggerParry()
         {
             SetState(Attack);
             Projectile.netUpdate = true;
             Projectile.ResetLocalNPCHitImmunity();
             var eaterMinions = (from p in Main.projectile
-                                     where p.active && p.owner == Projectile.owner && p.type == ModContent.ProjectileType<ScourgeOfTheCorruptor_Minion>()
-                                     select p);
+                                where p.active && p.owner == Projectile.owner && p.type == ModContent.ProjectileType<ScourgeOfTheCorruptor_Minion>()
+                                select p);
             foreach (var item in eaterMinions)
             {
-                for (var i =0; i < 20; i++)
-                    Dust.NewDustPerfect(item.Center, DustID.ScourgeOfTheCorruptor, Main.rand.NextVector2Circular(64,64), Scale: 1.2f);
-                SoundEngine.PlaySound(SoundID.DD2_BetsyFireballImpact, Projectile.Center);
+                float rot = Main.rand.NextFloat(MathHelper.TwoPi);
+                ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Scorch[1], item.Center, 20, ParticlePresets.ExplodeAndFade)
+                {
+                    Color = Color.Black,
+                    Scale = new(0.5f),
+                    Rotation = rot
+                });
+                ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Scorch[1], item.Center, 18, ParticlePresets.ExplodeAndFade)
+                {
+                    Color = new Color(88, 104, 66) with { A = 255 },
+                    UseTileLighting = false,
+                    Scale = new(0.3f),
+                    Rotation = rot
+                });
+                ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Scorch[1], item.Center, 17, ParticlePresets.ExplodeAndFade)
+                {
+                    Color = new Color(35, 40, 28),
+                    UseTileLighting = false,
+                    Scale = new(0.2f),
+                    Rotation = rot
+                });
+                SoundEngine.PlaySound(SoundID.DD2_BetsyFireballImpact with { MaxInstances = 1 }, Projectile.Center);
                 item.Resize(64, 64);
                 item.damage *= ScourgeOfTheCorruptor.EaterExplodeMult;
                 item.ResetLocalNPCHitImmunity();
@@ -207,7 +227,7 @@ namespace RootsBeta.Items.Weapons
 
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
-            if (State == Parry) 
+            if (State == Parry)
             {
                 Player.SetImmuneTimeForAllTypes(Player.longInvince ? 60 : 30); //TODO - make a util that can handle common iframe amounts automatically from enum
                 TriggerParry();
@@ -251,7 +271,30 @@ namespace RootsBeta.Items.Weapons
         public override void AI()
         {
             Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver4;
-            Dust.NewDustPerfect(Projectile.Center, DustID.ScourgeOfTheCorruptor, Vector2.Zero, Scale: 0.75f);
+
+
+            if (Projectile.numUpdates % 2 == 0)
+            {
+                ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Muzzle[3], Projectile.Center, 10, ParticlePresets.LinearShrinkAndFade)
+                {
+                    DrawLayer = DrawLayerSystem.DrawLayer.AfterNPCs,
+                    Color = new Color(35, 40, 28),
+                    UseTileLighting = true,
+                    Opacity = 0.15f,
+                    Scale = new(0.15f),
+                    Rotation = Projectile.rotation - MathHelper.PiOver4 * 3
+                });
+                ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Muzzle[3], Projectile.Center, 5, ParticlePresets.LinearShrinkAndFade)
+                {
+                    DrawLayer = DrawLayerSystem.DrawLayer.AfterProjectiles,
+                    Color = new Color(117, 145, 73) with { A = 125 },
+                    UseTileLighting = false,
+                    Opacity = 0.5f,
+                    Scale = new(0.05f),
+                    Rotation = Projectile.rotation - MathHelper.PiOver4 * 3
+                });
+            }
+            //Dust.NewDustPerfect(Projectile.Center, DustID.ScourgeOfTheCorruptor, Vector2.Zero, Scale: 0.75f);
         }
 
         public override void OnKill(int timeLeft)
@@ -261,6 +304,28 @@ namespace RootsBeta.Items.Weapons
                 for (var i = 0; i < ScourgeOfTheCorruptor.EaterCountPerSpear; i++)
                     Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2CircularEdge(4, 4), ModContent.ProjectileType<ScourgeOfTheCorruptor_Minion>(), (int)(Projectile.damage * (ScourgeOfTheCorruptor.EaterDmg / (float)ScourgeOfTheCorruptor.SpearDamage)), 0, Projectile.owner);
             }
+            float rot = Main.rand.NextFloat(MathHelper.TwoPi);
+            ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Scorch[1], Projectile.Center, 20, ParticlePresets.ExplodeAndFade)
+            {
+                Color = Color.Black,
+                UseTileLighting = false,
+                Scale = new(0.375f),
+                Rotation = rot
+            });
+            ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Scorch[1], Projectile.Center, 18, ParticlePresets.ExplodeAndFade)
+            {
+                Color = new Color(88, 104, 66),
+                UseTileLighting = false,
+                Scale = new(0.225f),
+                Rotation = rot
+            });
+            ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Scorch[1], Projectile.Center, 17, ParticlePresets.ExplodeAndFade)
+            {
+                Color = new Color(35, 40, 28),
+                UseTileLighting = false,
+                Scale = new(0.15f),
+                Rotation = rot
+            });
         }
         public override bool PreDraw(ref Color lightColor)
         {
@@ -317,7 +382,7 @@ namespace RootsBeta.Items.Weapons
 
         public override void AI()
         {
-            Projectile.rotation = -Projectile.velocity.ToRotation() - MathHelper.PiOver2;
+            Projectile.rotation = Projectile.velocity.ToRotation() - MathHelper.PiOver2;
 
             if (Projectile.frameCounter++ > 4)
             {
@@ -332,7 +397,27 @@ namespace RootsBeta.Items.Weapons
                 return;
             }
 
-            Dust.NewDustPerfect(Projectile.Center, DustID.ScourgeOfTheCorruptor, Vector2.Zero, Scale: 0.5f);
+            if (Projectile.numUpdates % 2 == 0)
+            {
+                ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Muzzle[3], Projectile.Center, 10, ParticlePresets.LinearShrinkAndFade)
+                {
+                    DrawLayer = DrawLayerSystem.DrawLayer.AfterNPCs,
+                    Color = new Color(35, 40, 28),
+                    UseTileLighting = true,
+                    Opacity = 0.5f,
+                    Scale = new(0.075f),
+                    Rotation = Projectile.rotation
+                });
+                ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Muzzle[3], Projectile.Center, 5, ParticlePresets.LinearShrinkAndFade)
+                {
+                    DrawLayer = DrawLayerSystem.DrawLayer.AfterProjectiles,
+                    Color = new Color(117, 145, 73) with { A = 125 },
+                    UseTileLighting = false,
+                    Opacity = 0.75f,
+                    Scale = new(0.035f),
+                    Rotation = Projectile.rotation
+                });
+            }
             int attackTarget = -1;
             Projectile.Minion_FindTargetInRange(1200, ref attackTarget, true, (enty, _) => enty is NPC nPC && Projectile.localNPCImmunity[enty.whoAmI] == 0 && nPC.CanBeChasedBy(Projectile));
             if (attackTarget >= 0)
@@ -356,6 +441,17 @@ namespace RootsBeta.Items.Weapons
             Projectile.netUpdate = true;
             attachTarget ??= target;
             Projectile.timeLeft = (int)MathHelper.Max(Projectile.timeLeft, ScourgeOfTheCorruptor.EaterHitrate * Projectile.MaxUpdates * 2);
+
+            var pos = Main.rand.NextVector2FromRectangle(target.Hitbox);
+            for (var i = 0; i < 5; i++)
+                ParticleSystem.SpawnParticle(new(ParticleTextures.Transparent.Star[5], pos, 30, ParticlePresets.LinearShrinkAndFade)
+                {
+                    Color = new Color(117, 145, 73) with { A = 0 },
+                    Velocity = Main.rand.NextVector2Circular(2, 2),
+                    UseTileLighting = false,
+                    Scale = new(Main.rand.NextFloat(0.025f, 0.075f)),
+                    Rotation = Main.rand.NextFloat(MathHelper.TwoPi)
+                });
         }
         public override bool PreDraw(ref Color lightColor)
         {
